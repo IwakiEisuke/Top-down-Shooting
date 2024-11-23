8c7107f086a3ff1d60b379565928feec759b1fba
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-11-24 04:41:02 +0900

    [update] 銃弾着弾時に即時破壊か跳弾をする挙動を実装

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,8 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] int _damage = 1;
     [SerializeField] float _existDuration = 10f;
+    [SerializeField] bool _destroyOnCollide;
+    [SerializeField] LayerMask _reflects;
     Rigidbody _rb;
     bool _isAttacked;
 
@@ -24,6 +26,20 @@ public class Bullet : MonoBehaviour
                 _isAttacked = true;
                 Destroy(gameObject);
             }
+        }
+
+        // layerMaskは何桁目に1があるか、gameObject.layerはレイヤー番号そのままの整数
+        if ((_reflects &= 1 << collision.gameObject.layer) != 0)
+        {
+            // 速度に反射ベクトルを代入
+            _rb.linearVelocity = Vector3.Reflect(-collision.relativeVelocity, collision.GetContact(0).normal);
+            transform.forward = _rb.linearVelocity; // 進行方向に向かせる
+        }
+
+        var layerMask = ~_reflects; // なぜかインラインだと反転できない…
+        if (_destroyOnCollide && (layerMask &= 1 << collision.gameObject.layer) != 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
