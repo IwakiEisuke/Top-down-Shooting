6a21c6cbda6f7002a696baf505af128d09abfc0a
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-11-27 16:48:00 +0900

    [add] トリガー範囲内の敵を検出し、検出した全ての敵が倒されたらイベントを実行するクラスを追加

---
+++ Assets/Scripts/AreaCondition.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AreaCondition : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _detectObjects;
+    [SerializeField] UnityEvent _allClearedEvent;
+    bool _activated;
+
+    private void Update()
+    {
+        foreach (GameObject obj in _detectObjects)
+        {
+            if (obj && obj.activeInHierarchy == true)
+            {
+                return;
+            }
+        }
+
+        if (!_activated)
+        {
+            Debug.Log($"{name} is all cleared");
+            _allClearedEvent.Invoke();
+            _activated = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_detectObjects.Contains(other.gameObject))
+        {
+            _detectObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_detectObjects.Contains(other.gameObject) || _detectObjects.Contains(other.transform.parent.gameObject))
+        {
+            _detectObjects.Remove(other.gameObject);
+        }
+    }
+}
