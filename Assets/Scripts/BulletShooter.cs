ed564d20d8952bd4f042b06fa26508af49cce07e
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-11-29 01:27:14 +0900

    [update] 弾が発射される方向をバラつかせるオプションを追加

--- Assets/Scripts/BulletShooter.cs
+++ Assets/Scripts/BulletShooter.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class BulletShooter : MonoBehaviour
 {
     [SerializeField] GameObject _bulletPref;
-    [SerializeField] float _shootForce = 10;
+    [SerializeField, FormerlySerializedAs("_shootForce")] float _bulletSpeed = 10;
+    [Tooltip("ËŒ‚s“®‚É’e‚ğŒ‚‚ÂŠÔŠu")]
     [SerializeField] float _bulletInterval = 0.2f;
+    [Tooltip("’e‚Ìƒoƒ‰‚Â‚«“x‡‚¢")]
+    [SerializeField] float _angleRandomness;
+    [SerializeField] bool _doRandomizeXAxisRotation;
     protected bool _isAttacking = false;
     float _timeSinceShoot = 0;
 
@@ -19,7 +24,11 @@ public class BulletShooter : MonoBehaviour
             bullet.transform.position = transform.position;
             bullet.transform.forward = targetPos - transform.position;
             var rb = bullet.GetComponent<Rigidbody>();
-            rb.linearVelocity = bullet.transform.forward * _shootForce;
+            // ’e‚ğƒoƒ‰‚Â‚©‚¹‚é
+            var xr = _doRandomizeXAxisRotation ? Random.Range(-_angleRandomness / 2, _angleRandomness / 2) : 0;
+            var yr = Random.Range(-_angleRandomness / 2, _angleRandomness / 2);
+            var forward = Quaternion.Euler(xr, yr, 0) * bullet.transform.forward;
+            rb.linearVelocity = forward * _bulletSpeed;
         }
     }
 
--- Assets/Scripts/EnemyBulletShooter.cs
+++ Assets/Scripts/EnemyBulletShooter.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class EnemyBulletShooter : BulletShooter
 {
     [SerializeField] string _playerName = "Player";
+    [Tooltip("一回の射撃で何秒間撃つか")]
     [SerializeField] float _shootingDuration = 1f;
+    [Tooltip("射撃行動をとれるようになるまでのインターバル")]
     [SerializeField] float _firingInterval = 3f;
     Transform _player;
     Coroutine _currentCoroutine;
