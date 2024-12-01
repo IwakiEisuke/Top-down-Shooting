39cc8550544bf634a2f02e18a7c19c5d73feb073
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-12-02 01:14:10 +0900

    [add] HealthRegenerator実装

---
+++ Assets/Scripts/Stats/HealthRegenerator.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(IHealth))]
+public class HealthRegenerator : MonoBehaviour, IDamageable
+{
+    [Tooltip("ダメージを受けてから再生を開始するまでの時間")]
+    [SerializeField, Min(0)] float _regenDelay = 2;
+    [Tooltip("毎秒の再生量")]
+    [SerializeField, Min(0.0001f)] float _regenSpeed = 1;
+    IHealth _health;
+
+    private void OnEnable()
+    {
+        _health = GetComponent<IHealth>();
+        InvokeRepeating(nameof(Regeneration), _regenDelay, 1 / _regenSpeed);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CancelInvoke();
+        InvokeRepeating(nameof(Regeneration), _regenDelay, 1 / _regenSpeed);
+    }
+
+    void Regeneration()
+    {
+        _health.Heal(1);
+    }
+}
