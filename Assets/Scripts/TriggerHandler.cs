fcbb260b225955dd6fe7cc9c0fc5d11ffc661be8
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-12-01 22:58:21 +0900

    [chore] 名称変更 ColliderEventHandler → TriggerHandler

--- Assets/Scripts/TriggerHandler.cs
+++ Assets/Scripts/TriggerHandler.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ColliderEventHandler : MonoBehaviour
+public class TriggerHandler : MonoBehaviour
 {
     [SerializeField] bool _oneShot = true;
     [SerializeField] UnityEvent OnTriggerEnterEvent;
