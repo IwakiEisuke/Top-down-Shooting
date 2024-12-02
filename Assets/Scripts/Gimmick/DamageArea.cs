e065f087047287541922b3d3b9e9af8669857ea0
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-12-03 03:07:47 +0900

    refactor(DamageArea): StatsManager参照箇所をIHealthに置き換え

--- Assets/Scripts/Gimmick/DamageArea.cs
+++ Assets/Scripts/Gimmick/DamageArea.cs
@@ -5,11 +5,11 @@ public class DamageArea : MonoBehaviour
 {
     [SerializeField] int _damage;
     [SerializeField] float _timeToDamage;
-    Dictionary<StatsManager, float> _timer = new();
+    Dictionary<IHealth, float> _timer = new();
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.TryGetComponent<StatsManager>(out var stats))
+        if (other.TryGetComponent<IHealth>(out var stats))
         {
             _timer.TryAdd(stats, 0);
             _timer[stats] += Time.deltaTime;
@@ -17,17 +17,15 @@ public class DamageArea : MonoBehaviour
             if (_timer[stats] > _timeToDamage)
             {
                 _timer[stats] = 0;
-                stats.DealDamage(_damage);
-                Debug.Log($"deal damage to {stats.name}");
+                stats.Damage(_damage);
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.TryGetComponent<StatsManager>(out var stats))
+        if (other.TryGetComponent<IHealth>(out var stats))
         {
-            //if (_timer.ContainsKey(stats))
             {
                 _timer.Remove(stats);
             }
