1a6f3faecbc37d2f3ec6efd4d197fb3ab4c6db10
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-12-21 03:32:55 +0900

    [update] Knockback追加

--- Assets/Scripts/CharacterControll/Knockback.cs
+++ Assets/Scripts/CharacterControll/Knockback.cs
@@ -1,21 +1,26 @@
+using DG.Tweening;
 using UniRx;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Knockback : MonoBehaviour
 {
     [SerializeField] ParticleSystem particle;
+    [SerializeField] float particleSpeed = 20;
     [SerializeField, Range(1, 100)] float resistance = 1;
     readonly ReactiveProperty<float> time = new();
     public bool IsKnockback { get; private set; }
+    Rigidbody rb;
+    float _originalRateOverTime;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        _originalRateOverTime = particle.emission.rateOverTimeMultiplier;
         time.Where(x => x <= 0)
-            .Subscribe(_ => 
+            .Subscribe(_ =>
             {
                 IsKnockback = false;
-                particle.Stop();
+                if (particle) particle.Stop();
             })
             .AddTo(this);
     }
@@ -23,12 +28,20 @@ public class Knockback : MonoBehaviour
     private void Update()
     {
         time.Value -= Time.deltaTime;
+        if (particle)
+        {
+            var speedRate = rb.linearVelocity.magnitude / particleSpeed;
+            var emission = particle.emission;
+            emission.rateOverTime = Mathf.Lerp(0, _originalRateOverTime, speedRate);
+            var main = particle.main;
+            main.startSizeMultiplier = Mathf.Lerp(0.5f, 1, speedRate);
+        }
     }
 
     public void ApplyKnockback(float t, Vector3 vel, Rigidbody rb)
     {
         IsKnockback = true;
-        particle.Play();
+        if (particle) particle.Play();
         this.time.Value = t / resistance;
         rb.AddForce(vel, ForceMode.Impulse);
     }
