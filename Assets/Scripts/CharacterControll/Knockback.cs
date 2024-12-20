a150eb29caeb724fddb86e121a19d607dc4ca119
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-12-21 04:06:48 +0900

    [update: Knockback] NavMeshAgentで移動するオブジェクトでも動作するようにしました

--- Assets/Scripts/CharacterControll/Knockback.cs
+++ Assets/Scripts/CharacterControll/Knockback.cs
@@ -1,20 +1,22 @@
-using DG.Tweening;
 using UniRx;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Knockback : MonoBehaviour
 {
     [SerializeField] ParticleSystem particle;
-    [SerializeField] float particleSpeed = 20;
+    [SerializeField, Range(0.01f, 1000)] float particleSpeed = 20;
     [SerializeField, Range(1, 100)] float resistance = 1;
     readonly ReactiveProperty<float> time = new();
     public bool IsKnockback { get; private set; }
     Rigidbody rb;
+    NavMeshAgent agent;
     float _originalRateOverTime;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
         _originalRateOverTime = particle.emission.rateOverTimeMultiplier;
         time.Where(x => x <= 0)
             .Subscribe(_ =>
@@ -30,7 +32,7 @@ public class Knockback : MonoBehaviour
         time.Value -= Time.deltaTime;
         if (particle)
         {
-            var speedRate = rb.linearVelocity.magnitude / particleSpeed;
+            var speedRate = agent ? agent.velocity.magnitude / particleSpeed : rb.linearVelocity.magnitude / particleSpeed;
             var emission = particle.emission;
             emission.rateOverTime = Mathf.Lerp(0, _originalRateOverTime, speedRate);
             var main = particle.main;
@@ -43,6 +45,8 @@ public class Knockback : MonoBehaviour
         IsKnockback = true;
         if (particle) particle.Play();
         this.time.Value = t / resistance;
-        rb.AddForce(vel, ForceMode.Impulse);
+        
+        if (agent) agent.velocity = vel / rb.mass;
+        else rb.AddForce(vel, ForceMode.Impulse);
     }
 }
