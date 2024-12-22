99f78484d6d0495c6b7490598b681f4b853d010b
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-12-22 21:48:18 +0900

    [update] NavMeshAgentへのKnockback挙動の安定性を向上

--- Assets/Scripts/CharacterControll/Knockback.cs
+++ Assets/Scripts/CharacterControll/Knockback.cs
@@ -7,32 +7,29 @@ public class Knockback : MonoBehaviour
     [SerializeField] ParticleSystem particle;
     [SerializeField, Range(0.01f, 1000)] float particleSpeed = 20;
     [SerializeField, Range(1, 100)] float resistance = 1;
-    readonly ReactiveProperty<float> time = new();
+    readonly ReactiveProperty<float> _time = new();
     public bool IsKnockback { get; private set; }
-    Rigidbody rb;
-    NavMeshAgent agent;
+    Rigidbody _rb;
+    NavMeshAgent _agent;
     float _originalRateOverTime;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        agent = GetComponent<NavMeshAgent>();
+        _rb = GetComponent<Rigidbody>();
+        _agent = GetComponent<NavMeshAgent>();
         _originalRateOverTime = particle.emission.rateOverTimeMultiplier;
-        time.Where(x => x <= 0)
-            .Subscribe(_ =>
-            {
-                IsKnockback = false;
-                if (particle) particle.Stop();
-            })
+        _time.Where(x => x <= 0 && IsKnockback)
+            .Subscribe(_ => ChangeState(false))
             .AddTo(this);
     }
 
     private void Update()
     {
-        time.Value -= Time.deltaTime;
+        _time.Value -= Time.deltaTime;
+        
         if (particle)
         {
-            var speedRate = agent ? agent.velocity.magnitude / particleSpeed : rb.linearVelocity.magnitude / particleSpeed;
+            var speedRate = (_agent ? _agent.velocity : _rb.linearVelocity).magnitude / particleSpeed;
             var emission = particle.emission;
             emission.rateOverTime = Mathf.Lerp(0, _originalRateOverTime, speedRate);
             var main = particle.main;
@@ -40,13 +37,26 @@ public class Knockback : MonoBehaviour
         }
     }
 
-    public void ApplyKnockback(float t, Vector3 vel, Rigidbody rb)
+    public void ApplyKnockback(float t, Vector3 impulse)
     {
-        IsKnockback = true;
-        if (particle) particle.Play();
-        this.time.Value = t / resistance;
-        
-        if (agent) agent.velocity = vel / rb.mass;
-        else rb.AddForce(vel, ForceMode.Impulse);
+        ChangeState(true);
+        _time.Value = t / resistance;
+        if (_agent) _agent.velocity = impulse / _rb.mass;
+        else _rb.AddForce(impulse / _rb.mass, ForceMode.Impulse);
+    }
+
+    private void ChangeState(bool isKnockback)
+    {
+        IsKnockback = isKnockback;
+        if (_agent)
+        {
+            _agent.ResetPath();
+        }
+
+        if (particle)
+        {
+            if (isKnockback) particle.Play();
+            else particle.Stop();
+        }
     }
 }
--- Assets/Scripts/CharacterControll/KnockbackSender.cs
+++ Assets/Scripts/CharacterControll/KnockbackSender.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using UnityEngine.AI;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Controllerに衝撃を与える能力を持たせるコンポーネント
@@ -8,17 +8,23 @@ public class KnockbackSender : MonoBehaviour
 {
     [SerializeField, Range(0, 100)] float _knockbackTime = 0.5f;
     Rigidbody _rb;
+    Vector3 _prevVel;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
+    private void FixedUpdate()
+    {
+        _prevVel = _rb.linearVelocity;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.TryGetComponent<Knockback>(out var k))
         {
-            k.ApplyKnockback(_knockbackTime, _rb.linearVelocity * _rb.mass, collision.rigidbody);
+            k.ApplyKnockback(_knockbackTime, _prevVel * _rb.mass);
         }
     }
 }
